c58c6b58eb880d53b878294f5bba9cc2829690ca
ekoontz@hiro-tan.org <ekoontz@hiro-tan.org>
2009-11-12 00:50:49 -0800

    next to do : import station info from bart_eta.xml

--- examples/bart/create.sql
+++ examples/bart/create.sql
@@ -1,3 +1,7 @@
 CREATE TABLE station (name TEXT,abbr CHAR(4) PRIMARY KEY);
 
 CREATE TABLE d_before (from_station TEXT,final_destination TEXT, distance INTEGER)");
+
+DROP TABLE IF EXISTS adjacent;
+
+CREATE TABLE adjacent(station_a CHAR(256),station_b CHAR(256));
