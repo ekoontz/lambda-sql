dad9a9f31353b125465cd55a034e0f3b0e19c74d
ekoontz@hiro-tan.org <ekoontz@hiro-tan.org>
2009-11-14 12:04:59 -0800

    add link and styles for output=xml

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function update_xml_url() {
+    var loc = document.location + ""; /* I can has indexOf()? */
+    if (loc.indexOf("?") == "-1") {
+	document.getElementById("as_xml_url").href=document.location + "?output=xml";
+    }
+    else {
+	document.getElementById("as_xml_url").href=document.location + "&output=xml";
+    }
+}
